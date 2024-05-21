2d33754d16c22cd52d85cb0898192464536a9152
coltec <coltec@coltec.com>
2024-05-21 19:14:03 -0300

    Initial Commit

---
+++ HabitJourney/HabitJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitJourneyApp.swift
+//  HabitJourney
+//
+//  Created by coltec on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
