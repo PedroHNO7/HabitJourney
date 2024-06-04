0a02659de69752b82150bd2584f69e7bacb2a7b5
coltec <coltec@coltec.com>
2024-06-04 20:40:57 -0300

    Cria o link para a página de adição & Deleta as imagens duplicadas

---
+++ HabitJourney/Views/AddScreen.swift
@@ -0,0 +1,18 @@
+//
+//  AddScreen.swift
+//  HabitJourney
+//
+//  Created by coltec on 04/06/24.
+//
+
+import SwiftUI
+
+struct AddScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddScreen()
+}
