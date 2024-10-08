9fc08d5dbad90814c7f6a00cd3f18e7a2e1f807b
coltec <coltec@coltec.com>
2024-10-08 20:50:30 -0300

    Adiciona funcionalidade de atribuir ID do Usuario ao Habito

--- HabitJourney/Views/LogSign/SignUpView.swift
+++ HabitJourney/Views/LogSign/SignUpView.swift
@@ -7,16 +7,14 @@ struct SignUpView: View {
     @State var userName: String
     @State var userEmail: String
     @State var userPassword: String
-    
+
+    @Environment(\.dismiss) var dismiss
     
     @State var show: Bool = false
     
     @State var nameField: FieldModel = FieldModel(value: "", fieldType: .name)
     @State var emailField: FieldModel = FieldModel(value: "", fieldType: .email)
     @State var passwordField: FieldModel = FieldModel(value: "", fieldType: .password)
-    
-    
-    @Environment(\.dismiss) var dismiss
     
     var body: some View {
         VStack{
@@ -25,12 +23,10 @@ struct SignUpView: View {
             Image(systemName: "pencil")
             Text("Cadastro").padding(.bottom)
             
-            
-            
             CustomTextField(fieldModel: $nameField)
                 .onSubmit {
                     nameField.onSubmitError()
-                }.autocapitalization(/*@START_MENU_TOKEN@*/.none/*@END_MENU_TOKEN@*/)
+                }
             
             
             CustomTextField(fieldModel: $emailField)
@@ -47,22 +43,23 @@ struct SignUpView: View {
             
             Button("Cadastrar"){
                 
-                var name = nameField.onValidate()
+                let name = nameField.onValidate()
                 
-                var email = emailField.onValidate()
-               
-                var password = passwordField.onValidate()
-            
+                let email = emailField.onValidate()
+                
+                let password = passwordField.onValidate()
+                
+                if name && email && password {
+                    let user = User(name: nameField.value, email: emailField.value, password: passwordField.value)
                     
-                let user = User(name: nameField.value, email: emailField.value, password: passwordField.value)
-                    
-                if (db.insertUser(user: user)){
-                    show = true
+                    if (db.insertUser(user: user)){
+                        show = true
+
+                        dismiss()
+                    }
+                } else {
+                    return
                 }
-                
-                
-                
-               
                    
             }.buttonStyle(.borderedProminent)
                 .padding()
@@ -70,7 +67,7 @@ struct SignUpView: View {
         .padding()
         .alert(isPresented: $show) {
             Alert( title: Text("Sucesso"),
-                   message: Text("\(userName) inserido com sucessso"),
+                   message: Text("\(userName) Inserido com sucessso"),
                    dismissButton: .default(Text("Ok"))
                    )
         }
